75daa996a0ee63a91fc24a288510aa8daa969aab
phgrey <phgrey@gmail.com>
2015-08-01 08:41:28 +0300

    one more field renaming

--- db/migrate/20150801005141_create_repositories.rb
+++ db/migrate/20150801005141_create_repositories.rb
@@ -2,7 +2,7 @@ class CreateRepositories < ActiveRecord::Migration
   def change
     create_table :repositories do |t|
       t.string :url
-      t.string :type
+      t.string :installer
       t.hstore :params
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20150801011225) do
 
   create_table "repositories", force: :cascade do |t|
     t.string   "url"
-    t.string   "type"
+    t.string   "installer"
     t.hstore   "params"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
